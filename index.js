57f3472b2fa63584c13d554567b65c900bed347a
Izky <51415144+elizky@users.noreply.github.com>
2020-01-30 16:05:53 -0300

    final

--- index.js
+++ index.js
@@ -1,21 +1,11 @@
 const express = require("express");
 const conectarDB = require("./config/db");
-// const cors = require('cors');
+const cors = require('cors');
 const {usuarios, auth, proyectos, tareas} = require('./routes')
-// crear el servidor
-// const app = express();
 
-// Cors
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  res.header("Allow", "GET");
-  next();
-});
+// crear el servidor
+const app = express();
+
 // conectar a la base de datos
 conectarDB();
 
