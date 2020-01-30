eb5874e37ddc19c82ada7eecf6fd18496bf9ae79
Izky <51415144+elizky@users.noreply.github.com>
2020-01-30 16:12:23 -0300

    cors problem

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const conectarDB = require("./config/db");
-const cors = require('cors');
+// const cors = require('cors');
 const {usuarios, auth, proyectos, tareas} = require('./routes')
 
 // crear el servidor
 const app = express();
 
+// Cors
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Allow", "GET");
+  next();
+});
+
 // conectar a la base de datos
 conectarDB();
 
 // Habilitar corse
-app.use(cors())
+// app.use(cors())
 
 //Habilitar express.json
 app.use(express.json({ extended: true }));
